3037e5577346d73286a5f7422f1431816f098101
Estudiantes <Estudiantes@Labing50404.udistrital.edu.co>
2018-02-09 11:54:15 -0500

    PRUEBA 3

---
+++ MI CALCULADORA/src/micalculadora/Calculadora.java
@@ -0,0 +1,26 @@
+package micalculadora;
+
+
+public class Calculadora {
+   int x;
+   int y;
+   int r;
+   void sumar (){
+       r = x+y;
+   }
+   void restar(){
+       if(x<y){
+           int a;
+           a=x;
+           x=y;
+           y=a;
+           if (x<0){
+               x=-x;
+               y=-y;
+               
+           }
+       }
+       
+       
+   }
+}
