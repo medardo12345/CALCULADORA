84e0d9e8f3fd73ba794a04b22ef716a58cb62924
Estudiantes <Estudiantes@Labing50404.udistrital.edu.co>
2018-02-09 11:47:00 -0500

    PRUEBA 3

--- MI CALCULADORA/src/micalculadora/Principal.java
+++ MI CALCULADORA/src/micalculadora/Principal.java
@@ -3,6 +3,9 @@ package micalculadora;
 
 public class Principal {
 public static void main(String[]args){
+    //crear un objeto clase calculadora
+    Calculadora miCalculadora; //Definicion
+    miCalculadora=new Calculadora();//Construir el objeto
     
 }
 }
